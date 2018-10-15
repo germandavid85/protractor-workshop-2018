5d589ebad35987f139131cf63486555921f83b44
German Potes <germandavid85@gmail.com>
2018-10-15 11:18:57 -0500

    Step 4 - disable promise manager (#4)

--- protractor/config.ts
+++ protractor/config.ts
@@ -8,5 +8,5 @@ export const config: Config = {
   },
   framework: 'jasmine',
   specs: [ '../test/spec.js' ],
-  seleniumAddress: 'http://localhost:4444/wd/hub'
+  SELENIUM_PROMISE_MANAGER: false
 };
