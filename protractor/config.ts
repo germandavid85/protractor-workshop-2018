e1db87d9256dbd779d64524a6751fc81251a8e40
German Potes <germandavid85@gmail.com>
2018-10-15 11:12:31 -0500

    Step 2 - improve test case (#2)

--- protractor/config.ts
+++ protractor/config.ts
@@ -1,6 +1,9 @@
-import { Config } from 'protractor';
+import { Config, browser } from 'protractor';
 
 export const config: Config = {
+  onPrepare: () => {
+    browser.ignoreSynchronization = true;
+  },
   framework: 'jasmine',
   specs: [ '../test/spec.js' ],
   seleniumAddress: 'http://localhost:4444/wd/hub'
