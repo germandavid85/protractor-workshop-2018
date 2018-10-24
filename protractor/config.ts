4c120fb83ede36a60b8ec92dc547b1e0fae4b9e8
German Potes <germandavid85@gmail.com>
2018-10-23 20:38:19 -0500

    Step 17: add chrome capabilities

--- protractor/config.ts
+++ protractor/config.ts
@@ -10,6 +10,13 @@ export const config: Config = {
   framework: 'jasmine',
   specs: ['../test/**/*.spec.js'],
   getPageTimeout: 30000,
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['disable-infobars=true', '--window-size=800,600'],
+      prefs: { credentials_enable_service: false }
+    }
+  },
   SELENIUM_PROMISE_MANAGER: false,
   jasmineNodeOpts: {
     defaultTimeoutInterval: 120000
